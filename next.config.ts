af113b92a490f9049977cf781271e6922186b5bb
Laerte Martins <laertemartins1971@gmail.com>
2026-06-05 13:13:19 -0300

    inclui versão na tela

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env: {
+    APP_VERSION: process.env.npm_package_version ?? "0.1.0",
+    BUILD_DATE: new Date().toISOString(),
+  },
 };
 
 export default nextConfig;
